80ab2ed95de0b8bea1c46c33beac988b83736783
kasmiii <48495453+kasmiii@users.noreply.github.com>
2023-06-21 17:59:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# last-form-email_validation
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/skp5ts)
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialExampleModule} from '../material.module';
+import {InputOverviewExample} from './input-overview-example';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+@NgModule({
+  declarations: [InputOverviewExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatChipsModule
+  ],
+  providers: [],
+  bootstrap: [InputOverviewExample],
+})
+export class AppModule {}
