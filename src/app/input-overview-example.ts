cc9ca58be922812196460fb195e8acb539249731
kasmiii <48495453+kasmiii@users.noreply.github.com>
2023-06-21 18:05:48 +0200

    2 files modified

--- src/app/input-overview-example.ts
+++ src/app/input-overview-example.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 /**
  * @title Basic Inputs
  */
@@ -8,7 +9,33 @@ import {Component} from '@angular/core';
   styleUrls: ['input-overview-example.css'],
   templateUrl: 'input-overview-example.html',
 })
-export class InputOverviewExample {}
+export class InputOverviewExample implements OnInit {
+  addOnBlurCC = true;
+  addOnBlurBCC = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  CCs: string[] = [];
+  BCCs: string[] = [];
+  selectedFiles: File[] = [];
+
+  //announcer = inject(LiveAnnouncer);
+  emailForm: FormGroup;
+  //matcher = new ErrorStateMatcher();
+
+  attachments: string[] = [];
+  ccAddresses: string[] = [];
+  bccAddresses: string[] = [];
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.emailForm = this.fb.group({
+      objectControl: ['', [Validators.required]],
+      commentControl: ['', [Validators.required]],
+    });
+  }
+}
 
 /**  Copyright 2023 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
